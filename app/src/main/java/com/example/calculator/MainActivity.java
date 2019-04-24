18989a90e22c6db44898979dc5a440b63542da13
TylorWolters <45407364+TylorWolters@users.noreply.github.com>
2019-04-24 16:01:43 -0500

    update

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,47 +11,53 @@ public class MainActivity extends AppCompatActivity {
             eightBtn, nineBtn, divideBtn, fourBtn, fiveBtn,
             sixBtn, multiplyBtn, oneBtn, twoBtn, threeBtn,
             minusBtn, zeroBtn, dotBtn, plusBtn;
-    EditText numberEntry, operatorChoice;
-    Double numOne, numTwo, numOneIsSet, numTotal;
+    String operatorChoice;
+    Double numOne, numTwo, numOneIsSet, isCalculated, numTotal;
     TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        result.findViewById(R.id.result);
-        ceBtn.findViewById(R.id.cE);
+        numOne = 0.0;
+        numTwo = 0.0;
+        isCalculated = 0.0;
+        numOneIsSet = 0.0;
+        result = findViewById(R.id.result);
+        ceBtn = findViewById(R.id.cE);
         ceBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 result.setText("");
-                numOne.equals(0.0);
-                numOneIsSet.equals(0.0);
-                numTwo.equals(0.0);
-                numberEntry.setText("");
+                numOne = 0.0;
+                numOneIsSet = 0.0;
+                numTwo = 0.0;
+                isCalculated = 0.0;
             }
         });
 
-        cBtn.findViewById(R.id.c);
+        cBtn = findViewById(R.id.c);
         cBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 result.setText("");
-                numberEntry.setText("");
+                if(isCalculated == 1.0){
+                    numOne = 0.0;
+                    numOneIsSet = 0.0;
+                    numTwo = 0.0;
+                }
             }
         });
 
-        equalsBtn.findViewById(R.id.equals);
+        equalsBtn = findViewById(R.id.equals);
         equalsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(numOneIsSet!=1.0)
-                {
-                    result.setText(numberEntry+"");
+                if(numOneIsSet!=1.0){
                 }
                 else
                 {
-                    numTwo.equals(numberEntry);
-                    switch (operatorChoice.toString()) {
+                    numTwo = Double.parseDouble(result.getText().toString());
+                    switch (operatorChoice) {
                         case "*":
                             numTotal = (numOne * numTwo);
                             result.setText(numTotal+"");
@@ -71,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
                             break;
                         case "^":
                             numTotal = Math.pow(numOne,numTwo);
-                            result.setText(numTotal+"");
+                            result.setText(numTotal + "");
                             break;
                         case "-":
                             numTotal = (numOne - numTwo);
@@ -80,155 +85,145 @@ public class MainActivity extends AppCompatActivity {
                         default:
                             result.setText("error!");
                     }
+                    isCalculated = 1.0;
                 }}
         });
-        divideBtn.findViewById(R.id.divide);
+        divideBtn = findViewById(R.id.divide);
         divideBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(numOneIsSet!=1.0) {
-                    numOne.equals(numberEntry);
-                    operatorChoice.setText("/");
-                    numOneIsSet.equals(1.0);
-                    numberEntry.equals(0.0);
+                    numOne = Double.parseDouble(result.getText().toString());
+                    operatorChoice = "/";
+                    numOneIsSet =1.0;
+                    result.setText("");
                 }
             }
         });
-        multiplyBtn.findViewById(R.id.multiply);
+        multiplyBtn = findViewById(R.id.multiply);
         multiplyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(numOneIsSet!=1.0) {
-                    numOne.equals(numberEntry);
-                    operatorChoice.setText("*");
-                    numOneIsSet.equals(1.0);
-                    numberEntry.equals(0.0);
+                    numOne = Double.parseDouble(result.getText().toString());
+                    operatorChoice = "*";
+                    numOneIsSet =1.0;
+                    result.setText("");
                 }
             }
         });
-        minusBtn.findViewById(R.id.minus);
+        minusBtn = findViewById(R.id.minus);
         minusBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(numOneIsSet!=1.0) {
-                    numOne.equals(numberEntry);
-                    operatorChoice.setText("-");
-                    numOneIsSet.equals(1.0);
-                    numberEntry.equals(0.0);
+                    numOne = Double.parseDouble(result.getText().toString());
+                    operatorChoice = "-";
+                    numOneIsSet = 1.0;
+                    result.setText("");
                 }
             }
         });
-        plusBtn.findViewById(R.id.plus);
+        plusBtn = findViewById(R.id.plus);
         plusBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(numOneIsSet!=1.0) {
-                    numOne.equals(numberEntry);
-                    operatorChoice.setText("+");
-                    numOneIsSet.equals(1.0);
-                    numberEntry.equals(0.0);
+                    numOne = Double.parseDouble(result.getText().toString());
+                    operatorChoice = "+";
+                    numOneIsSet = 1.0;
+                    result.setText("");
                 }
             }
         });
-        exponentBtn.findViewById(R.id.exponent);
+        exponentBtn = findViewById(R.id.exponent);
         exponentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(numOneIsSet!=1.0) {
-                    numOne.equals(numberEntry);
-                    operatorChoice.setText("^");
-                    numOneIsSet.equals(1.0);
-                    numberEntry.equals(0.0);
+                    numOne = Double.parseDouble(result.getText().toString());
+                    operatorChoice = "^";
+                    numOneIsSet =1.0;
+                    result.setText("");
                 }
             }
         });
 
-        dotBtn.findViewById(R.id.point);
+        dotBtn = findViewById(R.id.point);
         dotBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberEntry.append(".");
-                result.setText(numberEntry+"");
+                result.append(".");
             }
         });
-        zeroBtn.findViewById(R.id.zero);
+        zeroBtn = findViewById(R.id.zero);
         zeroBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberEntry.append("0");
-                result.setText(numberEntry+"");
+                result.append("0");
             }
         });
-        oneBtn.findViewById(R.id.one);
+        oneBtn = findViewById(R.id.one);
         oneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberEntry.append("1");
-                result.setText(numberEntry+"");
+                result.append("1");
             }
         });
-        twoBtn.findViewById(R.id.two);
+        twoBtn = findViewById(R.id.two);
         twoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberEntry.append("2");
-                result.setText(numberEntry+"");
+                result.append("2");
             }
         });
-        threeBtn.findViewById(R.id.three);
+        threeBtn = findViewById(R.id.three);
         threeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberEntry.append("3");
-                result.setText(numberEntry+"");
+                result.append("3");
             }
         });
-        fourBtn.findViewById(R.id.four);
+        fourBtn = findViewById(R.id.four);
         fourBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberEntry.append("4");
-                result.setText(numberEntry+"");
+                result.append("4");
             }
         });
-        fiveBtn.findViewById(R.id.five);
+        fiveBtn = findViewById(R.id.five);
         fiveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberEntry.append("5");
-                result.setText(numberEntry+"");
+                result.append("5");
             }
         });
-        sixBtn.findViewById(R.id.six);
+        sixBtn = findViewById(R.id.six);
         sixBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberEntry.append("6");
-                result.setText(numberEntry+"");
+                result.append("6");
             }
         });
-        sevenBtn.findViewById(R.id.seven);
+        sevenBtn = findViewById(R.id.seven);
         sevenBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberEntry.append("7");
-                result.setText(numberEntry+"");
+                result.append("7");
             }
         });
-        eightBtn.findViewById(R.id.eight);
+        eightBtn = findViewById(R.id.eight);
         eightBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberEntry.append("8");
-                result.setText(numberEntry+"");
+                result.append("8");
             }
         });
-        nineBtn.findViewById(R.id.nine);
+        nineBtn = findViewById(R.id.nine);
         nineBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberEntry.append("9");
-                result.setText(numberEntry+"");
+                result.append("9");
             }
         });
 
